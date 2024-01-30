74540aa486e395e10564c52635b228b422223fac
shivamp1998 <shivampandey98@gmail.com>
2024-01-30 22:22:47 +0530

    setting up canvas

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const secs = document.querySelector('.secs')
 const hoursInput = document.querySelector("input[name='hours']")
 const minutesInput = document.querySelector("input[name='minutes']")
 const secondsInput = document.querySelector("input[name='seconds']");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = "rgb(255,0,0)"
+ctx.fillRect(0,0,50,50)
+ctx.clearRect(0,0,25,25)
+
+ctx.beginPath();
+ctx.moveTo(51,5);
+ctx.lineTo(51,51)
+ctx.lineTo(102, 51);
+ctx.lineTo(102, 5);
+ctx.closePath();
+ctx.fillStyle="rgb(0,255,0)"
+ctx.fill()
 
 
 const formatVal = (val) => {
@@ -23,19 +38,18 @@ const interval = setInterval(() => {
     mins.innerText = formatVal(m);
     secs.innerText = formatVal(s)
     const date = new Date(new Date().getFullYear(),new Date().getMonth(), new Date().getDate(), h,m,s)
-    const alarmTime = localStorage.getItem('alarmDate');
+    const alarmTime = sessionStorage.getItem('alarmDate');
     if(alarmTime !== null && date.getTime() >= new Date(alarmTime).getTime()) {
         play()
     }
 }, 1000)
 
 const setAlarm = () => {
-    
     const h = hoursInput.value;
     const m = minutesInput.value;
     const s = secondsInput.value;
     const date = new Date(new Date().getFullYear(),new Date().getMonth(), new Date().getDate(), h,m,s)
-    localStorage.setItem('alarmDate', date)
+    sessionStorage.setItem('alarmDate', date)
 }
 
 const play = () => {
