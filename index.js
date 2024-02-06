37244f18a46bdf8185ad1efa099bfc3f3130b402
shivamp1998 <shivampandey98@gmail.com>
2024-02-06 23:52:33 +0530

    fixes

--- index.js
+++ index.js
@@ -6,39 +6,59 @@ canvas.width = 300
 const radius = 100
 const clockX = canvas.height / 2;
 const clockY =  canvas.width / 2 ;
+
+/* starting position of all the needles */
 let secondsHandAngle = -Math.PI / 2 + (new Date().getSeconds() + 1) * Math.PI / 30;
 let minutesHandAngle = -Math.PI / 2 + (new Date().getMinutes() ) * Math.PI / 30;
 let hoursHandlAngle = -Math.PI / 2 + ((new Date().getHours() % 12)) * Math.PI / 6;
-let number = 0;
 
-let i = 0;
+/*looping over values from 1 to 12 to give clock time values*/
+let number = 0;
 for(let i=-Math.PI / 2; i<=3*Math.PI / 2; i+= Math.PI / 6) {
-    ctx.strokeText(number == 0 ? 12 : number, clockX + (radius + 10) * Math.cos(i), clockY + (radius + 10) * Math.sin(i))
+    ctx.strokeStyle = "brown"
+    ctx.strokeText(number == 0 ? 12 : number, clockX + (radius + 10) * Math.cos(i), clockY + (radius + 15) * Math.sin(i))
     number++;
     if(number == 12) break;
 }
 
-setInterval(() => {
+const interval = setInterval(() => {
+    const h = new Date().getHours();
     const s = new Date().getSeconds();
     const m = new Date().getMinutes();
+    const alarmTime = sessionStorage.getItem("alarmTime")
+    console.log(alarmTime)
+    if(alarmTime !== null) {
+        console.log(alarmTime, new Date(alarmTime).getTime(), new Date().getTime())
+        if(new Date(alarmTime).getTime() === new Date().getTime()){
+            const audio = document.querySelector('.alarm');
+            audio.play();
+        }
+
+    }
+
+    //Outer Circle
     ctx.beginPath()
     ctx.arc(clockX, clockY,radius + 25, 0, 2 * Math.PI)
-    ctx.strokeStyle="pink"
+    ctx.strokeStyle="black"
     ctx.stroke()
     
+    //inner Circle
     ctx.beginPath();
     ctx.arc(clockX, clockY, radius, 0, 2 * Math.PI); 
     ctx.fillStyle = "beige";
     ctx.strokeStyle = "black"
+    ctx.lineWidth = 2
     ctx.stroke();
     ctx.fill(); 
 
     
-    
+    //cicle at the center of clock
     ctx.beginPath();
     ctx.arc(clockX, clockY, 5, -Math.PI / 2 , 3 * Math.PI / 2, true)
-    ctx.stroke();
+    ctx.strokeStyle="brown";
+    ctx.stroke()
 
+    //seconds hand needle
     ctx.beginPath();
     ctx.moveTo(clockX, clockY);
     const lineX = clockX + (radius - 10) * Math.cos(secondsHandAngle);
@@ -47,6 +67,7 @@ setInterval(() => {
     ctx.strokeStyle = "red"
     ctx.stroke();
 
+    //minute hand needle
     ctx.beginPath();
     ctx.moveTo(clockX, clockY);
     const lineMX = clockX + (radius-25) * Math.cos(minutesHandAngle);
@@ -57,6 +78,8 @@ setInterval(() => {
     if(s == 0) {
         minutesHandAngle += Math.PI / 30
     }
+
+    //hour hand needle
     ctx.beginPath();
     ctx.moveTo(clockX, clockY);
     const lineHX = clockX  + (radius - 50) * Math.cos(hoursHandlAngle);
@@ -69,3 +92,45 @@ setInterval(() => {
     }
     secondsHandAngle+= Math.PI / 30;
 }, 1000);
+
+/* setting alarm feature */
+
+const createOption = (element, optionsArray) => {
+    for(const op of optionsArray) {
+        const option = document.createElement('option');
+        option.innerHTML = (op % 10 === op) ? '0'+op : op
+        element.appendChild(option)
+    }
+}
+
+const hours = document.querySelector('.hours');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
+const meridian = document.querySelector('.meredian')
+
+hours.firstChild.innerHTML = new Date().getHours();
+minutes.firstChild.innerHTML = new Date().getMinutes();
+seconds.firstChild.innerHTML = new Date().getSeconds()
+
+
+const hoursRange = Array.from({length: 12}, (_, index) => index);
+const minutesSecRange = Array.from({length: 60}, (_, index) => index);
+
+createOption(hours, hoursRange);
+createOption(minutes, minutesSecRange);
+createOption(seconds, minutesSecRange)
+
+const handleAlarmSave = () => {
+    let h = +hours.value;
+    const m = +minutes.value;
+    const s = +seconds.value;
+    const md = meridian.value;
+    if(md === 'PM') h += 12
+    const alarmTime = new Date()
+    alarmTime.setHours(h);
+    alarmTime.setMinutes(m);
+    alarmTime.setSeconds(s)
+    sessionStorage.setItem('alarmTime', alarmTime)
+}
+
+
